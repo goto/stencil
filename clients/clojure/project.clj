0967055ee5a2aa78f83ab505a1b8930e2d30652a
gagan.dhand <gagan.deep@gojek.com>
2024-05-22 12:50:46 +0530

    feat: publish clojure client to artifactory

--- clients/clojure/project.clj
+++ clients/clojure/project.clj
@@ -1,4 +1,4 @@
-(defproject com.gotocompany/stencil-clj "0.5.0"
+(defproject com.gotocompany/stencil-clj "0.2.3-dev"
   :description "Stencil client for clojure"
   :url "https://github.com/goto/stencil"
   :license {:name "Apache 2.0"
@@ -8,4 +8,11 @@
   :plugins [[lein-cljfmt "0.7.0"]]
   :global-vars {*warn-on-reflection* true}
   :source-paths ["src"]
-  :repl-options {:init-ns stencil.core})
+  :repl-options {:init-ns stencil.core}
+  :deploy-repositories [["release" {:url "https://clojars.org/repo"
+                                    :username :env/ARTIFACTORY_USERNAME
+                                    :password :env/ARTIFACTORY_PASSWORD
+                                    :sign-releases false
+                                    }]
+                       ]
+)
