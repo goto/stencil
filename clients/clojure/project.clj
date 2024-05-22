aed9e10b2f7e6207d66b587599e1b780a3771795
gagan.dhand <gagan.deep@gojek.com>
2024-05-22 13:00:26 +0530

    feat: bump version

--- clients/clojure/project.clj
+++ clients/clojure/project.clj
@@ -1,4 +1,4 @@
-(defproject com.gotocompany/stencil-clj "0.2.3-dev"
+(defproject com.gotocompany/stencil-clj "0.2.2-dev"
   :description "Stencil client for clojure"
   :url "https://github.com/goto/stencil"
   :license {:name "Apache 2.0"
